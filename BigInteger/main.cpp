4814949a56afc833e6fd7b89e71020776b6b55f8
hassaneldesouky <hassaneldesouky@icloud.com>
2018-12-05 11:40:16 +0200

    Initial Commit

---
+++ BigInteger/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BigInteger
+//
+//  Created by Hassan El Desouky on 12/5/18.
+//  Copyright © 2018 Hassan El Desouky. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
